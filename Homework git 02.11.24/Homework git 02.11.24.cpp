93832656101fd4b2e264091fdf58df15a6e4ebdd
Sviatoslav <svyat23122008@gmail.com>
2024-11-07 23:15:32 +0200

    merge branch subtraction add

--- Homework git 02.11.24/Homework git 02.11.24.cpp
+++ Homework git 02.11.24/Homework git 02.11.24.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+
+#include <iostream> 
+using namespace std;
+
+
 
 int subtraction(int a, int b) {
 	return a - b;
+}
+int add(int a, int b) {
+    return a + b;
+}
+int main() {
+    cout << add(4, 7);
 }
 
 int main()
