1177a6edb8301562993968aa50c8edeeaf5a23dc
rrrr <Admin@AAAA>
2024-11-07 22:49:42 +0200

    add fuction

--- Homework git 02.11.24/Homework git 02.11.24.cpp
+++ Homework git 02.11.24/Homework git 02.11.24.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-
-int main()
-{
- 
-
-
-
+#include <iostream> 
+using namespace std;
+int add(int a, int b) {
+    return a + b;
 }
-
-
+int main() {
+    cout << add(4, 7);
+}
